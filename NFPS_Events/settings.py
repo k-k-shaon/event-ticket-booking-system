7aa74181f64f3c10d2c32cd8fb0074dd9d9fb208
k-k-shaon <kifayat.khan.shaon.656@gmail.com>
2025-12-22 03:32:53 +0600

    Add WhiteNoise and static settings, fix Google OAuth redirect

--- NFPS_Events/settings.py
+++ NFPS_Events/settings.py
@@ -32,6 +32,7 @@ USE_TZ = True
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
