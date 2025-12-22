fa5e56470bb0aaff0894f901e143b09a1a6642ac
k-k-shaon <kifayat.khan.shaon.656@gmail.com>
2025-12-22 19:02:59 +0600

    Settings for database connection

--- NFPS_Events/settings.py
+++ NFPS_Events/settings.py
@@ -60,13 +60,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'NFPS_Events.wsgi.application'
 
-# Database (SQLite for now)
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
